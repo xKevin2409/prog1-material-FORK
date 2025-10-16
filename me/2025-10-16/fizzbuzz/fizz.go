3f2068678ef518f25abfbf002eaf04d223e6d542
xKevin2409 <Kevin.roemer@heidelberg.com>
2025-10-16 14:43:17 +0200

    Add Fizz function and example test for FizzBuzz logic

---
+++ me/2025-10-16/fizzbuzz/fizz.go
@@ -0,0 +1,23 @@
+package fizzbuzz
+
+import (
+	"fmt"
+)
+
+// Fizz gibt Zahlen von 1 bis 30 aus und ersetzt dabei jede durch 3 teilbare Zahl durch fizz, jede durch 5 teilbare Zahl durch buzz und jede durch 3 und 5 teilbare Zahl durch fizzbuzz
+func Fizz() {
+	// Schleife von 1 bis 30, die prüft ob die zahlen durch 3 un/oder 5 teilbar sind
+	for i := 1; i < 31; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(i, "fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println(i, "fizz")
+		} else if i%5 == 0 {
+			fmt.Println(i, "buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+
+}
---
+++ me/2025-10-16/fizzbuzz/fizz_test.go
@@ -0,0 +1,16 @@
+package fizzbuzz
+
+func ExampleFizz() {
+	Fizz()
+	// Output:
+	// 1
+	// 2
+	// 3 fizz
+	// 4
+	// 5 buzz
+	// 6 fizz
+	// 7
+	// 8
+	// 9 fizz
+	// 10 buzz
+}
