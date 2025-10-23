8377594f8671ce13d8921ebc173cee851409aa4f
Reiner Huechting <dev@reinerhuechting.de>
2025-10-23 16:03:06 +0200

    Rekursives binfind.

--- code-vorlesung/2025-10-23/searching/bin_find_test.go
+++ code-vorlesung/2025-10-23/searching/bin_find_test.go
@@ -40,3 +40,21 @@ func ExampleBinFind2() {
 	// 6
 	// -1
 }
+
+func ExampleBinFindRek() {
+	l1 := []int{1, 6, 8, 10, 25, 42, 125, 277}
+	fmt.Println(BinFindRek(l1, 1))
+	fmt.Println(BinFindRek(l1, 277))
+	fmt.Println(BinFindRek(l1, 25))
+	fmt.Println(BinFindRek(l1, 10))
+	fmt.Println(BinFindRek(l1, 125))
+	fmt.Println(BinFindRek(l1, 3))
+
+	// Output:
+	// 0
+	// 7
+	// 4
+	// 3
+	// 6
+	// -1
+}
