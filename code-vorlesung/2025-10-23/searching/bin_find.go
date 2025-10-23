e4739ba27f2fe91ac8f9827a2aa3dc1f64db9636
Reiner Huechting <dev@reinerhuechting.de>
2025-10-23 15:26:06 +0200

    Mehr Find-Beispiele hinzugefügt.

--- code-vorlesung/2025-10-23/searching/bin_find.go
+++ code-vorlesung/2025-10-23/searching/bin_find.go
@@ -1,7 +1,7 @@
 package searching
 
-// / Liefert die Position von x in der Liste l,
-// / oder liefert -1, falls x nicht in l vorkommt.
+// Liefert die Position von x in der Liste l,
+// oder liefert -1, falls x nicht in l vorkommt.
 func BinFind1(l []int, x int) int {
 	links := 0
 	for len(l) > 0 {
@@ -24,6 +24,27 @@ func BinFind1(l []int, x int) int {
 			//l = l[mitte+1 : len(l)]
 			l = l[mitte+1:]
 			links += mitte + 1
+		}
+	}
+
+	return -1
+}
+
+// Liefert die Position von x in der Liste l,
+// oder liefert -1, falls x nicht in l vorkommt.
+func BinFind2(l []int, x int) int {
+	links := 0       // Hier beginnt der interessante Teil der Liste.
+	rechts := len(l) // Hier endet der interessante Teil der Liste.
+	for rechts > links {
+		mitte := (rechts-links)/2 + links
+
+		if l[mitte] == x {
+			return mitte
+		}
+		if x < l[mitte] {
+			rechts = mitte
+		} else {
+			links = mitte + 1
 		}
 	}
 
--- code-vorlesung/2025-10-23/searching/bin_find_test.go
+++ code-vorlesung/2025-10-23/searching/bin_find_test.go
@@ -21,3 +21,22 @@ func ExampleBinFind1() {
 	// 6
 	// -1
 }
+
+func ExampleBinFind2() {
+	l1 := []int{1, 6, 8, 10, 25, 42, 125, 277}
+
+	fmt.Println(BinFind2(l1, 1))
+	fmt.Println(BinFind2(l1, 277))
+	fmt.Println(BinFind2(l1, 25))
+	fmt.Println(BinFind2(l1, 10))
+	fmt.Println(BinFind2(l1, 125))
+	fmt.Println(BinFind2(l1, 3))
+
+	// Output:
+	// 0
+	// 7
+	// 4
+	// 3
+	// 6
+	// -1
+}
