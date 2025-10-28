2c3a46a0e7006fe21ba99189fe261a5f825dc638
xKevin2409 <Kevin.roemer@heidelberg.com>
2025-10-28 15:26:44 +0100

    Add BubbleUp function and example test cases for bubble sort

---
+++ me/2025-10-28/bubblesort/bubblesort.go
@@ -0,0 +1,14 @@
+package bubblesort
+
+//
+func BubbleUp(arr []int) bool {
+
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] > arr[i+1] {
+			arr[i], arr[i+1] = arr[i+1], arr[i]
+			return true
+		}
+	}
+
+	return false
+}
---
+++ me/2025-10-28/bubblesort/bubblesort_test.go
@@ -0,0 +1,24 @@
+package bubblesort
+
+import "fmt"
+
+func ExampleBubbleUp() {
+	a := []int{5, 1, 8, 3, 4}
+
+	fmt.Println(BubbleUp(a))
+	fmt.Println(a)
+
+	fmt.Println(BubbleUp(a))
+	fmt.Println(a)
+
+	fmt.Println(BubbleUp(a))
+	fmt.Println(a)
+
+	// Output:
+	// true
+	// [1 5 8 3 4]
+	// true
+	// [1 5 3 8 4]
+	// true
+	// [1 3 5 8 4]
+}
