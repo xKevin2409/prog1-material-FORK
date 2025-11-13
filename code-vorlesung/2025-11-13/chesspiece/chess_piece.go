fbf07170d81f54829abbbaca3c82e7336ed02d7f
Reiner Huechting <dev@reinerhuechting.de>
2025-11-13 16:05:18 +0100

    Anfang für Chesspiece-Aufgabe gemacht.

--- code-vorlesung/2025-11-13/chesspiece/chess_piece.go
+++ code-vorlesung/2025-11-13/chesspiece/chess_piece.go
@@ -11,9 +11,30 @@ Es geht nicht darum, Schachzüge akkurat in allen Details umzusetzen
 (z.B. keine Rochade oder andere komplexere Bedingungen).
 */
 
+type PType int
+
+const (
+	BISHOP PType = iota
+	KNIGHT
+	QUEEN
+	KING
+	ROOK
+	PAWN
+)
+
+type Colour int
+
+const (
+	WHITE Colour = iota
+	BLACK
+)
+
 // ChessPiece repräsentiert eine Schachfigur auf einem Spielfeld.
 type ChessPiece struct {
-	// TODO
+	pieceType PType
+	colour    Colour
+	row       int
+	column    int
 }
 
 // Methoden: TODO
@@ -23,3 +44,26 @@ type ChessPiece struct {
 // auf dieses Feld ziehen darf.
 // Besonderheiten wie Rochade oder im Weg stehende Figuren
 // Spielen keine Rolle.
+func (p ChessPiece) MoveAllowed(row, col int) bool {
+
+	// switch p.pieceType {
+	// case BISHOP: ...
+	// case KNIGHT: ...
+	// }
+
+	if p.pieceType == BISHOP {
+		// Diagonale von links unten nach rechts oben.
+		if row-col == p.row-p.column {
+			return true
+		}
+		// Diagonale von links oben nach rechts unten.
+		if row+col == p.row+p.column {
+			return true
+		}
+	}
+	if p.pieceType == KNIGHT {
+		/* ... */
+	}
+
+	return false
+}
