7bddea5465adc3992308debb7cb7ff1c4cc03282
Reiner Huechting <dev@reinerhuechting.de>
2025-11-13 15:14:57 +0100

    Stub für Schach-Aufgabe hinzugefügt.

---
+++ code-vorlesung/2025-11-13/chesspiece/chess_piece.go
@@ -0,0 +1,25 @@
+package chesspiece
+
+/* Ziel: Entwurf eines Datentyps für Schachfiguren.
+Es soll eine Datenstruktur für Schachfiguren entworfen werden,
+die eine Methode enthält, mit der geprüft werden kann,
+ob ein bestimmter Zug für diese Figur erlaubt ist.
+
+DISCLAIMER:
+Die Aufgabe soll den Umgang mit Structs und Consts/Enums üben.
+Es geht nicht darum, Schachzüge akkurat in allen Details umzusetzen
+(z.B. keine Rochade oder andere komplexere Bedingungen).
+*/
+
+// ChessPiece repräsentiert eine Schachfigur auf einem Spielfeld.
+type ChessPiece struct {
+	// TODO
+}
+
+// Methoden: TODO
+
+// MoveAllowed erwartet eine Feld-Angabe und liefert true,
+// falls die Figur nach den Bewegungsregeln beim Schach
+// auf dieses Feld ziehen darf.
+// Besonderheiten wie Rochade oder im Weg stehende Figuren
+// Spielen keine Rolle.
